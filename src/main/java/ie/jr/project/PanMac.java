503c7905949c799aafd3a3eeb80f883f249d8371
doheryjry0 <dohertyjr0@icloud.com>
2024-11-27 13:38:36 +0000

    Class Block, constructor && fields, HashSet and String tileMap added

--- src/main/java/ie/jr/project/PanMac.java
+++ src/main/java/ie/jr/project/PanMac.java
@@ -6,11 +6,61 @@ import java.util.HashSet;
 import javax.swing.*;
 
 public class PanMac extends JPanel {
+    class Block{
+        int x;
+        int y;
+        int width;
+        int height;
+        Image image;
+
+        int startX;
+        int startY;
+
+        Block(Image image, int x, int y, int width, int height){
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startX = x;
+            this.startY = y;
+        }
+    }
    private int rowCount = 21;
    private int columnCount = 19;
    private int tileSize = 32;
    private int boardWidth = columnCount * tileSize;
    private int boardHeight = rowCount * tileSize;
+
+   HashSet<Block> walls;
+   HashSet<Block> foods;
+   HashSet<Block> ghosts;
+   Block pacman;
+
+   private String[] tileMap = {
+            "XXXXXXXXXXXXXXXXXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X                 X",
+            "X XX X XXXXX X XX X",
+            "X    X       X    X",
+            "XXXX XXXX XXXX XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXrXX X XXXX",
+            "O       bpo       O",
+            "XXXX X XXXXX X XXXX",
+            "OOOX X       X XOOO",
+            "XXXX X XXXXX X XXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X  X     P     X  X",
+            "XX X X XXXXX X X XX",
+            "X    X   X   X    X",
+            "X XXXXXX X XXXXXX X",
+            "X                 X",
+            "XXXXXXXXXXXXXXXXXXX"
+   };
+
 
    private Image wallImage;
    private Image blueGhostImage;
@@ -19,9 +69,9 @@ public class PanMac extends JPanel {
    private Image pinkGhostImage;
 
    private Image pacmanUpImage;
-    private Image pacmanDownImage;
-    private Image pacmanLeftImage;
-    private Image pacmanRightImage;
+   private Image pacmanDownImage;
+   private Image pacmanLeftImage;
+   private Image pacmanRightImage;
 
 
     PanMac(){
@@ -38,5 +88,6 @@ public class PanMac extends JPanel {
         pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
         pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
         pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+
     }
 }
